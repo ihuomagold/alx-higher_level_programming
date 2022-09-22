c15b93dda4b2857e12758d02b5ddab6634d78469
ihuomagold <ihuomagold53@gmail.com>
2022-09-22 15:35:15 -0700

    added tasks 0-5

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    num = len(sys.argv)
+    if num == 1:
+        print("{} {}" .format(num-1, arguments.))
+    elif num == 2:
+        print("{} {}" .format(num-1, argument:))
+    else:
+        print("{} {}" .format(num-1, arguments:))
+
+    for n in range(1, num):
+        print("{} : {}" .format(n, sys.argv[n]))
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    result = 0
+    for n in range(n, len(sys.argv)):
+        result += int(sys.argv[n])
+        print("{}".format(result))
