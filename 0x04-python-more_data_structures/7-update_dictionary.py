f575229915eaba86c50472a1a7fd95d7342d40dd
ihuomagold <ihuomagold53@gmail.com>
2022-09-30 11:07:29 -0700

    syntax and indentation corrections

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -4,6 +4,6 @@ def update_dictionary(a_dictionary, key, value):
         a_dictionary[key] = value
     else:
         for i in a_dictionary:
-             if i == key:
-                 a_dictionary[i] = value
+            if i == key:
+                a_dictionary[i] = value
     return a_dictionary
