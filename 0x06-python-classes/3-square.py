86e5332c9861d6897b8f4584ca1a5a59a5eeee14
ihuomagold <ihuomagold53@gmail.com>
2022-10-11 05:16:05 -0700

    taks 0-6

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+class Square:
+    """Represents a square
+    Private instance attribute: size
+    Instantiation with optional size
+    Public instance method that returns the current square area.
+    """
+
+    def __init__(self, size=0):
+        """Initialises the data."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns current square area."""
+        return self.__size ** 2
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+class Square:
+    """Represents a square
+    Private instance attribute: size
+    Instantiation with optional size
+    Public instance method: def area(self)
+    """
+
+    def __init__(self, size=0):
+        """Initializes the data"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieve size"""
+        return self.__size
+
+    @property.setter
+    def size(self, value):
+        """Sets it"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size ** 2
