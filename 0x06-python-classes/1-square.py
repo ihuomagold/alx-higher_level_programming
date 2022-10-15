912869f4528656712ecce4710c14c8272841a2c2
ihuomagold <ihuomagold53@gmail.com>
2022-10-15 11:34:42 -0700

    fixed class name

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """A Python program on Classes"""
 
-class User:
+class Square:
     """Represents a class that defines a square
     Private instance attribute: size
     Instantiation with size (no type/value verification).
